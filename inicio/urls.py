2e2f18f016fce237ee92bfcfeec72892a7a444cc
Hebe <hebecar1753@gmail.com>
2023-04-20 00:31:10 -0300

    se agregaron templates

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from inicio import views
 
+app_name= 'inicio'
+
 
 urlpatterns = [
-    path ('', views.inicio)    
+    path ('', views.inicio, name = 'inicio'), 
+    path ('nosotros/', views.nosotros, name= 'Nosotros'),   
 ]
 
--- inicio/views.py
+++ inicio/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def inicio (request):
     return render (request, 'inicio/inicio.html')
+
+
+def nosotros (request):
+    return render (request , 'inicio/nosotros.html')
+
